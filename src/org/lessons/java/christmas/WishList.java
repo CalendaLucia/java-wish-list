be512c7325b5ab8c2a997af60de25d7577785ec6
CalendaLucia <calendalucia4@gmail.com>
2023-06-09 15:40:14 +0200

    aggiunta controlli su input utente

--- src/org/lessons/java/christmas/WishList.java
+++ src/org/lessons/java/christmas/WishList.java
@@ -1,5 +1,6 @@
 package org.lessons.java.christmas;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,28 +8,42 @@ import java.util.Scanner;
 
 public class WishList {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
+
         //CREO L ARRAY CHE SARà LA LISTA DEI REGALI
         List <String> wishList = new ArrayList<>();
 
         //CHIEDERE ALL UTENTE IL NOME DEL REGALO DA INSERIRE FIN QUANDO NON DECIDE DI FERMARSI
         boolean stop = false;
+
         do {
             System.out.print("Scrivi il nome del regalo: ");
             String nomeRegalo = scanner.nextLine();
             wishList.add(nomeRegalo);
+
           //AD OGNI ITERAZIONE MOSTRO LA LUNGHEZZA DELLA LISTA
             System.out.println("Nella tua lista ci sono " + wishList.size() + " regali");
             System.out.println("Vuoi continuare ad inserire regali? Si/No");
             String risposta = scanner.nextLine();
+
             if (risposta.equalsIgnoreCase("no")) {
-                 stop = true;
+                stop = true;
                 Collections.sort(wishList);//Ordino la lista
                 System.out.println("Ecco la tua lista di regali!!");
                 for (String regalo : wishList) {
                     System.out.println(regalo.toUpperCase());
                 }
             }
+            else if (risposta.isEmpty() || !risposta.equalsIgnoreCase("si")) {
+                System.out.println("Valore non valido! Scrivi si o no");
+                System.out.println("Vuoi continuare ad inserire regali? Si/No");
+                 scanner.nextLine();
+
+            }
+
+
         } while (!stop);
 
         scanner.close();
